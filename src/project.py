37dc3dd1ed80aaae713eadea8d0c51aa82feb008
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-04 11:00:39 -0600

    Pseudocode for phase 1 of project

---
+++ src/project.py
@@ -0,0 +1,25 @@
+# Pseudocode for STEP 1 OF PROJECT
+
+# Locator names
+    # Center
+    # Left Side
+
+# Parenting Locators
+    # Center
+    # Left Arm
+    # Left Leg
+
+# Making Joints Out Of Locators
+    # Center
+    # Left
+
+# Parenting Joints
+
+# Orienting Joints
+    # Center Chain
+    # Left Arm Chain
+    # Left Leg Chain
+
+# Mirror Left Joints to Right
+    # Mirror Arm
+    # Mirror leg
