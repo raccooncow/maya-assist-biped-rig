050e445c2cf16efdfbea6dd3579556ba65660b7d
Raquel Baca <raquelbaca4002@gmail.com>
2025-12-04 12:08:28 -0600

    Fixed indentation

--- src/project.py
+++ src/project.py
@@ -5,43 +5,43 @@ def build_skeleton():
     import maya.cmds as cmds
 # Locator names
     # Center
-center_locs = [
-    "pelvis_LOC",
-    "spine01_LOC",
-    "spine02_LOC",
-    "spine03_LOC",
-    "neck_LOC",
-]
+    center_locs = [
+        "pelvis_LOC",
+        "spine01_LOC",
+        "spine02_LOC",
+        "spine03_LOC",
+        "neck_LOC",
+    ]
     # Left Side
-left_locs = [
-    "L_shoulder_LOC",
-    "L_elbow_LOC",
-    "L_wrist_LOC",
-    "L_hip_LOC",
-    "L_knee_LOC",
-    "L_ankle_LOC",
-]
+    left_locs = [
+        "L_shoulder_LOC",
+        "L_elbow_LOC",
+        "L_wrist_LOC",
+        "L_hip_LOC",
+        "L_knee_LOC",
+        "L_ankle_LOC",
+    ]
 
 # Parenting Locators
-parents = {
-    # Center
-    "spine01_LOC": "pelvis_LOC",
-    "spine02_LOC": "spine01_LOC",
-    "spine03_LOC": "spine02_LOC",
-    "neck_LOC": "spine03_LOC",
-    # Left Arm
-    "L_shoulder_LOC": "spine03_LOC",
-    "L_elbow_LOC": "L_shoulder_LOC",
-    "L_wrist_LOC": "L_elbow_LOC",
-    # Left Leg
-    "L_hip_LOC": "pelvis_LOC",
-    "L_knee_LOC": "L_hip_LOC",
-    "L_ankle_LOC": "L_knee_LOC",
+    parents = {
+        # Center
+        "spine01_LOC": "pelvis_LOC",
+        "spine02_LOC": "spine01_LOC",
+        "spine03_LOC": "spine02_LOC",
+        "neck_LOC": "spine03_LOC",
+        # Left Arm
+        "L_shoulder_LOC": "spine03_LOC",
+        "L_elbow_LOC": "L_shoulder_LOC",
+        "L_wrist_LOC": "L_elbow_LOC",
+        # Left Leg
+        "L_hip_LOC": "pelvis_LOC",
+        "L_knee_LOC": "L_hip_LOC",
+        "L_ankle_LOC": "L_knee_LOC",
     }
 
 # Making Joints Out Of Locators
-joint_map = {}
-def create_joint(loc):
+    joint_map = {}
+    def create_joint(loc):
         if not cmds.objExists(loc):
             return None
         pos = cmds.xform(loc, q=True, ws=True, t=True)
@@ -50,16 +50,16 @@ def create_joint(loc):
         cmds.select(clear=True)
         return jnt
     # Center
-for loc in center_locs:
-    joint_map[loc] = create_joint(loc)
+    for loc in center_locs:
+        joint_map[loc] = create_joint(loc)
     # Left
-for loc in left_locs:
-    joint_map[loc] = create_joint(loc)
+    for loc in left_locs:
+        joint_map[loc] = create_joint(loc)
 
 # Parenting Joints
-for loc, parent_loc in parents.items():
-    if joint_map.get(loc) and joint_map.get(parent_loc):
-        cmds.parent(joint_map[loc], joint_map[parent_loc])
+    for loc, parent_loc in parents.items():
+        if joint_map.get(loc) and joint_map.get(parent_loc):
+            cmds.parent(joint_map[loc], joint_map[parent_loc])
 
 # Orienting Joints
     # Center Chain
